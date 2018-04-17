8a90e1e0ac4bb27da7e7ffe32661981a3ffd8583
Emran <xmanemran@gmail.com>
2018-04-17 21:55:53 +0600

    Add basic items

--- App.js
+++ App.js
@@ -1,58 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+	Platform,
+	StyleSheet,
+	Text,
+	View,
+	FlatList,
+	Dimensions
 } from 'react-native';
+const {width, height} = Dimensions.get('screen');
+import Intro from './Intro'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+export default class App extends Component{
+	
+	state = {
+		width,
+		height,
+		activeIndex: 0,
+	};
+	
+	
+	_renderItem(data){
+		return (
+			<View style={{backgroundColor: '#'+(Math.random()*0xFFFFFF<<0).toString(16), flex: 1, width, height, justifyContent: 'center', alignItems:'center'}}>
+				<Text>{data.item.title}</Text>
+			</View>
+		)
+		
+	}
+	
+	
+	componentDidMount() {
+		console.log('ok')
+		const { width, height } = Dimensions.get('window');
+		if (width !== this.state.width || height !== this.state.height) {
+			// Set new width to update rendering of pages
+			this.setState({ width, height });
+			// Set new scroll position
+			const func = () => { this.flatList.scrollToOffset({ offset: this.state.activeIndex * width, animated: false }) }
+			Platform.OS === 'android' ? setTimeout(func, 0) : func();
+		}
+	}
+	
+	
+	_onMomentumScrollEnd = (e) => {
+		console.log(e.nativeEvent);
+		const offset = e.nativeEvent.contentOffset.x;
+		const newIndex = Math.round(offset / this.state.width);
+		if (newIndex === this.state.activeIndex) {
+			return;
+		}
+		const lastIndex = this.state.activeIndex;
+		this.setState({ activeIndex: newIndex });
+		this.props.onSlideChange && this.props.onSlideChange(newIndex, lastIndex);
+	}
+	
+	
+	render() {
+		return (
+			<FlatList
+				ref={(ref) => { this.flatList = ref; }}
+				data={[{title: "Emran"}, {title: "Mahmiud"}, {title: "Farha"}, {title: "Tamim"}, {title: "Shamim"}]}
+				horizontal={true}
+				showsHorizontalScrollIndicator={false}
+				bounces={false}
+				pagingEnabled
+				renderItem={this._renderItem}
+				keyExtractor={(data, id)=>id+''}/>
+			
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
